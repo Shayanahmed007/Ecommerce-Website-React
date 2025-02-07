a917346faa113ee8e8ea58e0f332bd6316ba07f2
Shayanahmed007 <140046277+Shayanahmed007@users.noreply.github.com>
2025-02-07 16:13:30 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+import { Container } from 'postcss';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: "class",
+  theme: {
+    extend: {
+      colors:{
+        primary: "#fea928",
+        secondary: "#ed8900",
+      },
+      fontFamily:{
+        container:{
+          centre: true,
+          padding:{
+            DEFAULT: "1rem",
+            sm:"3rem",
+          }
+
+        }
+      }
+    },
+  },
+  plugins: [],
+}
